2355848d763c605beb8735b8fd29849b947fd9a5
Ayush Agarwal <agayush456@gmail.com>
2021-01-09 17:20:41 +0530

    coding of EMICard Class

---
+++ finance-II/src/main/java/com/lti/entity/EMICard.java
@@ -0,0 +1,71 @@
+package com.lti.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EMI_CARD")
+public class EMICard {
+
+	@Id
+	@GeneratedValue
+	@Column(name = "CARD_NO")
+	private long cardNo;
+
+	@Column(name = "Validity")
+	private LocalDate validityOfCard;
+
+	@Column(name = "Type")
+	private String cardType;
+
+	@Column(name = "Balance")
+	private double balance;
+
+	private Registration registration;
+
+	public long getCardNo() {
+		return cardNo;
+	}
+
+	public void setCardNo(long cardNo) {
+		this.cardNo = cardNo;
+	}
+
+	public LocalDate getValidityOfCard() {
+		return validityOfCard;
+	}
+
+	public void setValidityOfCard(LocalDate validityOfCard) {
+		this.validityOfCard = validityOfCard;
+	}
+
+	public String getCardType() {
+		return cardType;
+	}
+
+	public void setCardType(String cardType) {
+		this.cardType = cardType;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public Registration getRegistration() {
+		return registration;
+	}
+
+	public void setRegistration(Registration registration) {
+		this.registration = registration;
+	}
+
+}
