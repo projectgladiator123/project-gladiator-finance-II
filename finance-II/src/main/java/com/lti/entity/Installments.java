288e373e9abb5584376b5fd5dd9bcdbc40ae816a
Ayush Agarwal <agayush456@gmail.com>
2021-01-09 18:07:52 +0530

    change in Installments class

--- finance-II/src/main/java/com/lti/entity/Installments.java
+++ finance-II/src/main/java/com/lti/entity/Installments.java
@@ -6,61 +6,88 @@ import javax.persistence.*;
 
 @Entity
 public class Installments {
+	
 	  @Id
 	  @GeneratedValue
 	  @Column(name="Id")
-	  private int id;
-	  @Column(name="Purchase_Id")
-	  Purchases Pid=new Purchases();
+	  private int installmentId;
+	  
+	
 	  @Column(name="Payment_Date")
 	  private LocalDate paymentDate;
+	  
 	  @Column(name="Amount_Paid")
-	  private long amountPaid;
-	  @Column(name="Due_Date")
+	  private double amountPaid;
+	  
+	  @Column(name="Due_date")
 	  private LocalDate dueDate;
+	  
 	  @Column(name="Status")
 	  private String status;
-	public int getId() {
-		return id;
+	  
+	  
+	  private Purchases purchases;
+
+
+	public int getInstallmentId() {
+		return installmentId;
 	}
-	public void setId(int id) {
-		this.id = id;
+
+
+	public void setInstallmentId(int installmentId) {
+		this.installmentId = installmentId;
 	}
-	
-	public Purchases getPid() {
-		return Pid;
-	}
-	public void setPid(Purchases pid) {
-		Pid = pid;
-	}
+
+
 	public LocalDate getPaymentDate() {
 		return paymentDate;
 	}
+
+
 	public void setPaymentDate(LocalDate paymentDate) {
 		this.paymentDate = paymentDate;
 	}
-	public long getAmountPaid() {
+
+
+	public double getAmountPaid() {
 		return amountPaid;
 	}
-	public void setAmountPaid(long amountPaid) {
+
+
+	public void setAmountPaid(double amountPaid) {
 		this.amountPaid = amountPaid;
 	}
+
+
 	public LocalDate getDueDate() {
 		return dueDate;
 	}
+
+
 	public void setDueDate(LocalDate dueDate) {
 		this.dueDate = dueDate;
 	}
+
+
 	public String getStatus() {
 		return status;
 	}
+
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
-	 
 
-	
-	   
+
+	public Purchases getPurchases() {
+		return purchases;
+	}
+
+
+	public void setPurchases(Purchases purchases) {
+		this.purchases = purchases;
+	}
 	  
+	
 	  
 }
