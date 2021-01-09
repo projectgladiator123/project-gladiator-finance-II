aa1096f5b75dff53f0b2c4c59c828a4a0838267e
Ayush Agarwal <agayush456@gmail.com>
2021-01-09 11:18:52 +0530

    trying with push

---
+++ finance-II/src/main/java/com/lti/entity/Registration.java
@@ -0,0 +1,21 @@
+package com.lti.entity;
+
+public class Registration {
+
+	private int customerId;
+	private String customerName;
+	
+	public int getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(int customerId) {
+		this.customerId = customerId;
+	}
+	public String getCustomerName() {
+		return customerName;
+	}
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+	
+}
