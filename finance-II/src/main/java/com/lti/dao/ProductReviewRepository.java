571c0429fb865a275ad95e30c61d9834b0f2817c
Tanmay <tripathitanmay18@gmail.com>
2021-01-11 09:05:04 +0530

    made service and repository class.

--- finance-II/src/main/java/com/lti/dao/ProductReviewRepository.java
+++ finance-II/src/main/java/com/lti/dao/ProductReviewRepository.java
@@ -19,6 +19,7 @@ public class ProductReviewRepository extends GenericRepository{
 		@PersistenceContext
 		EntityManager entityManager;
 		
+		@SuppressWarnings("unchecked")
 		public List<ProductReview> fetchAllReviews(){		
 			return entityManager.createQuery("select pr from ProductReview pr").getResultList();
 		}
@@ -28,6 +29,16 @@ public class ProductReviewRepository extends GenericRepository{
 			entityManager.createQuery("delete from ProductReview pr where pr.reviewId = :reviewId").setParameter("reviewId", reviewId).executeUpdate();
 	
 	
-}
+		}
+		@SuppressWarnings("unchecked")
+		public List<ProductReview> fetchReviewbyUser(int userId) {
+			return entityManager.createNamedQuery("select ur from ProductReview where review(ur.userId)=: rt").setParameter("rt",userId).getResultList();
+			
+		}
+		@SuppressWarnings("unchecked")
+		public List<ProductReview> fetchReviewbyProduct(int productId){
+			return entityManager.createNamedQuery("select por from ProductReview where review(ur.productId)= :rm").setParameter("rm",productId).getResultList();
+			
+		}
 
 }
--- finance-II/src/main/java/com/lti/service/ProductReviewService.java
+++ finance-II/src/main/java/com/lti/service/ProductReviewService.java
@@ -1,6 +1,15 @@
 package com.lti.service;
 
+import java.util.List;
+
+import com.lti.entity.ProductReview;
+
+
 public interface ProductReviewService {
-	public ProductReviewService review(String review,int rating);
+	public List<ProductReview> showAllR();
+	public List<ProductReview> fetchReviewbyUser();
+	public List<ProductReview> fetchReviewbyProduct();
+	
+	public void deleteUserReview(int userId);
 
 }
