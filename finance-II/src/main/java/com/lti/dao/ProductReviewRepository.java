6f38d2f4c9da1c6c4c9d57bd6c5174b76a70bd78
Tanmay <tripathitanmay18@gmail.com>
2021-01-12 18:40:54 +0530

    working product review fetching.

--- finance-II/src/main/java/com/lti/dao/ProductReviewRepository.java
+++ finance-II/src/main/java/com/lti/dao/ProductReviewRepository.java
@@ -27,12 +27,12 @@ public class ProductReviewRepository extends GenericRepository{
 		}
 		@SuppressWarnings("unchecked")
 		public List<ProductReview> fetchReviewbyUser(int userId) {
-			return entityManager.createNamedQuery("select p from ProductReview p where p.registration.userId =: rt").setParameter("rt",userId).getResultList();
+			return entityManager.createQuery("select p from ProductReview p where p.registration.userId =: rt").setParameter("rt",userId).getResultList();
 			
 		}
 		@SuppressWarnings("unchecked")
 		public List<ProductReview> fetchReviewbyProduct(int productId){
-			return entityManager.createNamedQuery("select p from ProductReview p where p.product.productId = :rm").setParameter("rm",productId).getResultList();
+			return entityManager.createQuery("select p from ProductReview p where p.product.productId = :rm").setParameter("rm",productId).getResultList();
 			
 		}
 
